617c3f0d274e815f4cc7445e44ff4d25dab2732c
Ann <iykh499@gmail.com>
2021-03-24 17:27:41 +0200

    add index

---
+++ technicalTask/sushi/assets/js/dist/main.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    arrows: false,
+    mobileFirst: true
+  });
+});
+/* $(document).ready(function() {
+    var slider = $('#slider').lightSlider({
+        item: 1,
+        auto: true,
+        slideMove: 1,
+        speed: 5000,
+        loop: true,
+        controls: false,
+        slideWidth: 900
+    });
+    $('.prev').click(function(){
+        slider.goToPrevSlide(); 
+    });
+    $('.next').click(function(){
+        slider.goToNextSlide(); 
+    });
+}); */
